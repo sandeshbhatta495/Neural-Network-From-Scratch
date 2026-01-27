57f2002bf9945a9c6b79f128bdf9def89bf26fbd
sandeshbhatta495 <bhattasandesh148@gmail.com>
2026-01-27 23:42:10 +0545

    Adding the loss of categroical

--- Ch 05 - Calculating Network Output/final.py
+++ Ch 05 - Calculating Network Output/final.py
@@ -9,42 +9,45 @@ class Layer_Dense:
         # Initialize weights and biases
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
-        # Forward pass
-        def forward(self, inputs):
-            # Calculate output values from inputs, weights and biases
-            self.output = np.dot(inputs, self.weights) + self.biases
-    # ReLU activation
-    class Activation_ReLU:
-        # Forward pass
-        def forward(self, inputs):
-            # Calculate output values from inputs
-            self.output = np.maximum(0, inputs)
+    # Forward pass
+    def forward(self, inputs):
+        # Calculate output values from inputs, weights and biases
+        self.output = np.dot(inputs, self.weights) + self.biases
 
-    # Softmax activation
-    class Activation_Softmax:
-        # Forward pass
-        def forward(self, inputs):
-            # Get unnormalized probabilities
-            exp_values = np.exp(inputs - np.max(inputs,axis=1,keepdims=True))
-            # Normalize them for each sample
-            probabilities = exp_values / np.sum(exp_values, axis=1,
-            keepdims=True)
-            self.output = probabilities
-    # Common loss class
-    class Loss:
+# ReLU activation
+class Activation_ReLU:
+    # Forward pass
+    def forward(self, inputs):
+        # Calculate output values from inputs
+        self.output = np.maximum(0, inputs)
+
+# Softmax activation
+class Activation_Softmax:
+    # Forward pass
+    def forward(self, inputs):
+        # Get unnormalized probabilities
+        exp_values = np.exp(inputs - np.max(inputs,axis=1,keepdims=True))
+        # Normalize them for each sample
+        probabilities = exp_values / np.sum(exp_values, axis=1,
+        keepdims=True)
+        self.output = probabilities
+
+# Common loss class
+class Loss:
     # Calculates the data and regularization losses
     # given model output and ground truth values
-        def calculate(self, output, y):
-            # Calculate sample losses
-            sample_losses = self.forward(output, y)
-            # Calculate mean loss
-            data_loss = np.mean(sample_losses)
-            # Return loss
-            return data_loss
-    # Cross-entropy loss
-    class Loss_CategoricalCrossentropy(Loss):
-        # Forward pass
-        def forward(self, y_pred, y_true):
+    def calculate(self, output, y):
+        # Calculate sample losses
+        sample_losses = self.forward(output, y)
+        # Calculate mean loss
+        data_loss = np.mean(sample_losses)
+        # Return loss
+        return data_loss
+
+# Cross-entropy loss
+class Loss_CategoricalCrossentropy(Loss):
+    # Forward pass
+    def forward(self, y_pred, y_true):
         # Number of samples in a batch
         samples = len(y_pred)
         # Clip data to prevent division by 0
